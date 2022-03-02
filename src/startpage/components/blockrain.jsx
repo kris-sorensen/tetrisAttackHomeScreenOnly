331f6884aea97c6b2ab77ed19154c8e17f91bb71
Kris Sorensen <Krismsorensen@gmail.com>
2022-03-01 23:29:25 -0500

    drop() method.

--- src/startpage/components/blockrain.jsx
+++ src/startpage/components/blockrain.jsx
@@ -12,12 +12,13 @@ class Blockrain extends Component {
     dropArr: [], // each piece will be obj with{x: y: color:}
   };
   componentDidMount() {
-    this.place();
+    for (let i = 0; i < 40; i++) {
+      this.place();
+      this.drop();
+    }
   }
 
   render() {
-    // console.log("my board", this.state.board);
-    // console.log(tetriminos);
     const array = [];
     for (let i = 0; i <= this.state.rows; i++) {
       array.push(
@@ -53,17 +54,26 @@ class Blockrain extends Component {
     const tetrimino = tetriminos[Math.floor(Math.random() * tetriminos.length)];
     // pick random spot to place tetrimino
     const placeHere = this.find(tetrimino.cordinates);
-    console.log("placeHere:", placeHere);
-    // grab tetrimino object at that location(cant just be a refrence). will go into dropObj with the appropriate amount added to y for right location.
-        //grap array from object and color, send into dropObj as object
-    const dropObj = Object.create()
-    dropObj.color = tetrimino.color;
 
-    dropObj.xy = tetrimino.cordinates[];
+    //update board to show that there will be a tetrimino dropping to the placeHere location
+    for (let i = 0; i < tetrimino.cordinates.length; i++) {}
 
-    // add appropriate amount to y to hit top location
-    // update matrice with -1 at final spot ()
-    // place into dropObj with key of resting location
+    // create data to place into dropArr so that it will drop and paint this tetrimino while tetrimino is moving on board.
+    const dropRefrence = [];
+    for (let i = 0; i < tetrimino.cordinates.length; i++) {
+      const x = tetrimino.cordinates[i][0];
+      const y = tetrimino.cordinates[i][1];
+      //update board with final resting location of tetrmino
+      this.state.board[x + placeHere[0]][y + placeHere[1]] = -1;
+      // console.log(x + placeHere[0], x + placeHere[1]);
+      dropRefrence.push([x, y + placeHere[1]]);
+    }
+    dropRefrence.push(tetrimino.color);
+    dropRefrence.push(placeHere);
+    // push to dropArr
+    this.state.dropArr.push(dropRefrence);
+    // console.log(this.state.dropArr);
+    // console.log(this.state.board);
   }
 
   find(cordinates) {
@@ -81,7 +91,7 @@ class Blockrain extends Component {
           // looping over cordinates of tetrimino to see if it will fit
           for (let z = 1; z < cordinates.length; z++) {
             if (board[x + cordinates[z][0]][y + cordinates[z][1]] !== 0) {
-              console.log("xy", x, y);
+              // console.log("xy", x, y);
               freeSpace = false;
             }
           }
@@ -91,19 +101,50 @@ class Blockrain extends Component {
         }
       }
       // if found a spot return a random spot to place tetrimino.
-      console.log(array);
+
       if (array.length > 0)
         return array[Math.floor(Math.random() * array.length)];
     }
   }
 
   drop() {
+    const { board } = this.state;
+    const { dropArr } = this.state;
+    // add 1 to x to drop piece
+    for (let i = 0; i < dropArr.length; i++) {
+      if (dropArr[i][0][0] === dropArr[i][5][0]) {
+        // -2
+        for (let j = 0; j < 4; j++) {
+          const xx = dropArr[i][j][0];
+          const yy = dropArr[i][j][1];
+          board[xx][yy] = -2;
+        }
+        dropArr.shift(); // shift CHECK d
+        console.log("dropArr:", dropArr);
+      }
+      //access x
+      for (let x = 0; x < 4; x++) {
+        // console.log("drop Arr 1", dropArr[i][x]);
+        dropArr[i][x][0] = dropArr[i][x][0] + 1;
+        // console.log("drop Arr 2", dropArr[i][x]);
+      }
+      // dropArr[i][0];
+      // console.log("dropArr:", dropArr);
+      // console.log("dropArr spot:", dropArr[i][0][0]);
+    }
+    console.log("this.board = : ", board);
+    // loop ..
+    // if first === last turn this.state.board(at [0]) ---> -2 and all other spots, and unshift the array. it should always be the first element.
+
+    // take 1 from x
+
     //Order: drop, paint, check for matching key values and delete those.
-      // use map to update all values
+    // use map to update all values
     // if y is negative skip paint.
     // update dom based on x's and y's of each property in object
     //add 1 to all x's and check for piece keys and value cordinates matching.
     // if match change matrice to -2 and trigger rowComplete()?
+    // delete from arr
   }
 
   paint() {
